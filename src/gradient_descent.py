943580701c1c03e87c346d7c07a7ff996f18b1a6
carlfre <carlfrederiknk@gmail.com>
2022-10-18 15:27:04 +0200

    Start implementing GradientDescent class. Implement adagrad + rmsprop

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -1,6 +1,129 @@
 #%%
+import math
+
 import numpy as np
 from regression_cost_funcs import OLSCost, RidgeCost
+from abc import ABC, abstractmethod
+
+
+class GradientDescent:
+
+    def __init__(
+        self,
+        batch_size = None,
+        momentum_param = 0,
+        adagrad = False,
+        adam = False,
+        rmsprop = False,
+        store_extra=False
+    ):  
+        # Batch size of None corresponds to gradient descent
+        self.batch_size = batch_size
+
+        # Momentum discount factor
+        self.momentum_param = momentum_param
+        self.momentum = 0
+
+        self.use_adagrad = adagrad
+        self.use_adam = adam
+        self.use_rmsprop = rmsprop
+        self.store_extra = store_extra
+
+        # TODO: make adjustable
+        self.rms_param = 0.9
+
+
+    def train(self, X, w, y, model, learning_rate, n_epochs):
+        self.X = X
+        self.w = w.reshape(-1, 1)
+        self.y = y.reshape(-1, 1)
+        self.model = model
+        self.eta = learning_rate
+
+        if self.batch_size is None:
+            batch_size = len(y)
+        else:
+            batch_size = self.batch_size
+
+        if self.store_extra:
+            # Store cost function and weight values for each epoch
+            self.costs = np.zeros(n_epochs + 1)
+            self.weights = np.zeros((n_epochs + 1, len(w)))
+            # Store cost and weight values before training
+            self.costs[0] = model.cost(X, self.w, self.y)
+            self.weights[0] = w.flatten()
+        
+        for epoch in range(1, n_epochs + 1):
+            # Shuffle data
+            if self.batch_size:
+                idx = np.random.permutation(len(self.y))
+                self.X = self.X[idx]
+                self.y = self.y[idx]
+            # For each batchm, update weights
+            for batch in range(math.ceil(len(y) / self.batch_size)):
+                self.X_batch = self.X[batch * batch_size: (batch + 1) * batch_size]
+                self.y_batch = self.y[batch * batch_size: (batch + 1) * batch_size]
+                self.w = self.update()
+            # Store cost and weight values after each epoch
+            if self.store_extra:
+                    self.costs[epoch] = model.cost(X, self.w, y)
+                    self.weights[epoch] = self.w.flatten()
+        
+        return self.w
+
+    def delta_w(self, X, w, y, model, eta):
+        return self.momentum * self.momentum_param - eta * model.gradient(X, w, y)
+
+    def adagrad(self):
+        if not hasattr(self, "G"):
+            self.G = np.zeros(( len(self.w), 1))
+        #print(self.G.shape, (self.model.gradient(self.X_batch, self.w, self.y_batch)**2).shape)
+        #print((self.G + self.model.gradient(self.X_batch, self.w, self.y_batch)**2).shape)
+        self.G += self.model.gradient(self.X_batch, self.w, self.y_batch)**2
+        return self.eta / np.sqrt(self.G)
+    
+    def rmsprop(self):
+        if not hasattr(self, "v"):
+            self.v = np.zeros((len(self.w), 1))
+        self.v = self.rms_param * self.v + (1 - self.rms_param) * self.model.gradient(self.X_batch, self.w, self.y_batch)**2
+        return self.eta / np.sqrt(self.v)
+
+    #def sampler(self):
+    #    if not hasattr(self, "y") or not hasattr(self, "X"):
+    #        raise ValueError("X and y have not been set.")
+    #        
+    #    if self.batch_size == None:
+    #        return (self.X, self.y)
+    #
+    #    elif 0 < self.batch_size < 1:
+    #        n_samples = self.batch_size
+    #        idx = np.random.choice(len(self.y), n_samples, replace=False)
+    #        return (self.X[idx], self.y[idx])
+    #
+    #    else:
+    #        raise ValueError("Batch size must be in interval (0, 1]")
+    
+    def update(self):
+        X = self.X_batch
+        w = self.w
+        y = self.y_batch
+        model = self.model
+        eta = self.eta
+
+        if self.use_adagrad:
+            eta_star = self.adagrad()
+        elif self.use_rmsprop:
+            eta_star = self.rmsprop()
+        else:
+            eta_star = eta
+
+        delta_w = self.delta_w(X, w, y, model, eta_star)
+        self.momentum = delta_w
+        return w + delta_w
+        # TODO: Handle adam
+        
+
+
 
 def gradient_descent(
     X, 
@@ -8,7 +131,7 @@ def gradient_descent(
     w, 
     model, 
     n_iter, 
-    learning_rate,
+    eta,
     compute_extra=True,
 ):
     """Gradient descent algorithm.
@@ -25,7 +148,7 @@ def gradient_descent(
         Cost function with gradient method
     n_iter: int
         Number of iterations
-    learning_rate: float
+    eta: float
         Learning rate
     compute_extra: bool
         Whether to return cost function values and weight path
@@ -49,7 +172,7 @@ def gradient_descent(
         # Compute the gradient
         grad = model.gradient(X, w, y)
         # Update w
-        w = w - learning_rate * grad
+        w = w - eta * grad
         # Compute the cost function if specified
         if compute_extra:
             cost[i] = model.cost(X, w, y)
@@ -67,7 +190,7 @@ def momentum_gradient_descent(
     w, 
     model, 
     n_iter, 
-    learning_rate, 
+    eta, 
     gamma=0.9,
     compute_extra=False,
 ):
@@ -85,7 +208,7 @@ def momentum_gradient_descent(
         Cost function with gradient method
     n_iter: int
         Number of iterations
-    learning_rate: float
+    eta: float
         Learning rate
     gamma: float
         Momentum parameter
@@ -112,7 +235,7 @@ def momentum_gradient_descent(
         # Compute the gradient
         grad = model.gradient(X, w, y)
         # Update v
-        v = gamma * v + learning_rate * grad
+        v = gamma * v + eta * grad
         # Update w
         w = w - v
         # Compute the cost function if specified
@@ -133,7 +256,7 @@ def stochastic_gradient_descent(
     model, 
     n_iter, 
     batch_size, 
-    learning_rate, 
+    eta, 
     compute_extra=False,
 ):
     """Stochastic gradient descent algorithm.
@@ -152,7 +275,7 @@ def stochastic_gradient_descent(
         Number of iterations
     batch_size: int
         Number of samples to use in each iteration
-    learning_rate: float
+    eta: float
         Learning rate
     compute_extra: bool
         Whether to return cost function values and weight path
@@ -178,7 +301,7 @@ def stochastic_gradient_descent(
         # Compute the gradient
         grad = model.gradient(X[indices], w, y[indices])
         # Update w
-        w = w - learning_rate * grad
+        w = w - eta * grad
 
         if compute_extra:
             cost[i] = model.cost(X, w, y)
@@ -196,7 +319,7 @@ def stochastic_gradient_descent_with_momentum(
     model,
     n_iter,
     batch_size,
-    learning_rate,
+    eta,
     gamma=0.9,
     compute_extra=False,
 ):
@@ -216,7 +339,7 @@ def stochastic_gradient_descent_with_momentum(
         Number of iterations
     batch_size: int
         Number of samples to use in each iteration
-    learning_rate: float
+    eta: float
         Learning rate
     gamma: float
         Momentum parameter
@@ -245,7 +368,7 @@ def stochastic_gradient_descent_with_momentum(
         # Compute the gradient
         grad = model.gradient(X[indices], w, y[indices])
         # Update v
-        v = gamma * v + learning_rate * grad
+        v = gamma * v + eta * grad
         # Update w
         w = w - v
 
@@ -383,6 +506,42 @@ def stochastic_gradient_descent_with_momentum_example():
     plt.legend()
     plt.show()
 
+def gradient_descent_class_example():
+    import matplotlib.pyplot as plt
+    N = 100
+    X = np.random.randn(N, 2)
+    y = 2 * X[:, 0] + 3 * X[:, 1] + np.random.randn(N)
+
+    y = y.reshape(-1, 1)
+
+    # Initialize weights
+    w = np.zeros((2)).reshape(-1, 1)
+
+    gd = GradientDescent(momentum_param=0.5, batch_size=20, store_extra=True, rmsprop=True)
+    w = gd.train(X, w, y, OLSCost(), 0.5, 5)
+    print("True weights: [2, 3]")
+    print(f"Final weights: {w}")
+    print(f"Final cost: {gd.costs[-1]}")
+    print(gd.costs)
+
+    model = OLSCost()
+    print(w)
+    print(y.shape)
+    print(model.cost(X, w, y))
+
+    # Plot cost function
+    plt.plot(gd.costs)
+    plt.show()
+
+    # Plot weight path
+    plt.plot(gd.weights[:, 0], label="w0")
+    plt.plot(gd.weights[:, 1], label="w1")
+    plt.legend()
+    plt.show()
+
+
+
 
 if __name__ == "__main__":
-    stochastic_gradient_descent_with_momentum_example()
+    gradient_descent_class_example()
+
