d7499c9910916840c24e3b29632b502ad47760de
Leander Parton <blparton@student.matnat.uio.no>
2022-10-28 17:31:16 +0200

    Add analysis of batch size in g_d_a

--- src/Testing.py
+++ src/Testing.py
@@ -5,3 +5,15 @@ import regression_tools as rt
 import numpy as np
 import matplotlib.pyplot as plt
 
+z_pred_arr = np.zeros([201, 125])
+
+z_test = np.array([0.1, 1, 2.2])
+
+print(np.shape(z_pred_arr))
+print(np.shape(z_test))
+
+
+#201, 125
+#125
+
+
