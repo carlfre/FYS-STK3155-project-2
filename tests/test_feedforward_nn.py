59c041fef73eaed581df9a26bf0f74ef65f65bd4
carlfre <carlfrederiknk@gmail.com>
2022-11-06 17:53:18 +0100

    Add forward prop test

--- tests/test_feedforward_nn.py
+++ tests/test_feedforward_nn.py
@@ -46,25 +46,39 @@ def test_unflatten_weights():
     assert np.array_equal(biases[2], np.array([[5]]))
 
 
+# TODO: add tests for activation functions (particularly derivatives)
+def test_relu():
+    pass
+
+
+def test_sigmoid():
+    pass
+
+
+def test_leaky_relu():
+    pass
+
+
 def test_forward_propagation():
-    nn = NeuralNetwork([2, 2, 2, 1], "relu")
-    nn.weights = [np.array([[1, 2], [3, 4]]), np.array([[5, 6], [7, 8]]), np.array([[9, 10]])]
-    nn.biases = [np.array([[1, 2]]), np.array([[3, 4]]), np.array([[5]])]
-    X = np.array([[1, 2], [3, 4]])
-    wb = nn.wb()
+    X = np.array([[1, 2]])
+    nn = NeuralNetwork([2, 2, 2, 1], "sigmoid")
 
-    activations, zs = None, None#nn._forward_propagation(X)
-    assert np.array_equal(activations[0], X)
-    assert np.array_equal(activations[1], np.array([[13, 16], [29, 36]]))
-    assert np.array_equal(activations[2], np.array([[69, 80], [157, 184]]))
-    assert np.array_equal(activations[3], np.array([[389, 410], [901, 952]]))
-    assert np.array_equal(zs[0], np.array([[1, 2], [3, 4]]))
-    assert np.array_equal(zs[1], np.array([[13, 16], [29, 36]]))
-    assert np.array_equal(zs[2], np.array([[69, 80], [157, 184]]))
-    assert np.array_equal(zs[3], np.array([[389, 410], [901, 952]]))
+    nn.weights = [np.array([[1, 2], [3, 4]]) * 0.1, np.array([[5, 6], [7, 8]]) * 0.1, np.array([[9, 10]]).T * 0.1]
+    nn.biases = [np.array([[1, 2]])* 0.1, np.array([[3, 4]]) * 0.1, np.array([[5]]) * 0.1]
+    nn.X = X
 
+    activations, zs = nn._forward_propagation()
 
-
+    atol = 1e-4
+    # Compare with hand-caluclated values
+    assert np.allclose(zs[0], X, atol=atol)
+    assert np.allclose(activations[0], X, atol=atol)
+    assert np.allclose(zs[1], np.array([[0.8, 1.2]]), atol=atol)
+    assert np.allclose(activations[1], np.array([[0.68997448, 0.76852478]]), atol=atol)
+    assert np.allclose(zs[2], np.array([[1.182954586, 1.428804512]]), atol=atol)
+    assert np.allclose(activations[2], np.array([[0.76547863027, 0.80671497654]]), atol=atol)
+    assert np.allclose(zs[3], np.array([[1.995645743783]]), atol=atol)
+    assert np.allclose(activations[3], np.array([[0.880339150445242]]), atol=atol)
 
 
 
